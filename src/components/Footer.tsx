4dff5198515477423347b4902d7bd627422847a7
Lorza-masonska <maciek.mendygral@gmail.com>
2025-07-12 20:15:03 +0200

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import AppVersion from "./AppVersion";
+
+export default function Footer() {
+  return (
+    <footer>
+      <AppVersion />
+      {/* inne rzeczy w stopce */}
+    </footer>
+  );
+}
